0c573e6f021347b88984dd643cdd2c026258cce0
NycolasSM <nycolassantana00@gmail.com>
2022-03-12 11:41:34 -0300

    initial config: vite, eslint, pretterr

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Portfolio from "./pages/Portfolio";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
---
+++ src/pages/Portfolio/index.tsx
@@ -0,0 +1,5 @@
+function Portfolio() {
+  return <div>Teste</div>;
+}
+
+export default Portfolio;
