cec3a8049f033a391fdfc8c76cfe7ace791af8fd
NycolasSM <nycolassantana00@gmail.com>
2022-03-14 16:38:33 -0300

    improve responsive

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -5,13 +5,21 @@ import React from "react";
 type Props = {
   title: string;
   description: string;
+  maxWidth?: number;
 };
 
-const SectionTitle: React.FC<Props> = ({ title, description }) => {
+const SectionTitle: React.FC<Props> = ({ title, description, maxWidth }) => {
   return (
     <div className="section__title_wrapper">
       <h3 className="section__title">{title}</h3>
-      <h2 className="section__description">{description}</h2>
+      <h2
+        className="section__description"
+        style={{
+          maxWidth: `${maxWidth}px`,
+        }}
+      >
+        {description}
+      </h2>
     </div>
   );
 };
--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -14,7 +14,11 @@ function Portfolio() {
       </h2>
       <main>
         <section className="portfolio__section">
-          <SectionTitle title="Introdução" description="Um pouco sobre mim." />
+          <SectionTitle
+            title="Introdução"
+            description="Falando um pouco sobre mim."
+            maxWidth={600}
+          />
           <div className="about__me__container">
             <span>
               Desenvolvedor Web com experiência em diversos projetos próprios e
@@ -24,12 +28,25 @@ function Portfolio() {
               buscando sempre estar pronto para superar qualquer desafio!
             </span>
             <div className="about__me__cards">
-              <div className="about__me__card"></div>
-              <div className="about__me__card"></div>
-              <div className="about__me__card"></div>
-              <div className="about__me__card"></div>
+              <div className="about__me__card projects__count">
+                <p>20+</p>
+                <p>projetos</p>
+              </div>
+              <div className="about__me__card years__experience">
+                <p>1+</p>
+                <p>Experiencia</p>
+              </div>
+              <div className="about__me__card commits_count">
+                <p>340+</p>
+                <p>Commits no GitHub</p>
+              </div>
+              <div className="about__me__card passion__percent">
+                <p>100%</p>
+                <p>paixão</p>
+              </div>
             </div>
           </div>
+          <h3 className="my__technologies__stack">Minhas Stacks</h3>
         </section>
       </main>
     </>
