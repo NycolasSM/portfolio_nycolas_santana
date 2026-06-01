3eb9009288236d961432d780bd13284b4f856860
NycolasSM <nycolassantana00@gmail.com>
2026-06-01 20:42:00 -0300

    update

--- src/components/Hero/components/ProfileTitle/index.tsx
+++ src/components/Hero/components/ProfileTitle/index.tsx
@@ -1,22 +1,2 @@
-import "./style.css";
-
-import { BrowserView, MobileView } from "react-device-detect";
-
-const ProfileTitle = () => {
-  return (
-    <>
-      <BrowserView>
-        <div className="profile__Title">
-          <h3>Desenvolvedor Web/Mobile</h3>
-        </div>
-      </BrowserView>
-      <MobileView>
-        <div className="profile__Title--mobile">
-          <h3>Desenvolvedor Web/Mobile</h3>
-        </div>
-      </MobileView>
-    </>
-  );
-};
-
-export default ProfileTitle;
+const ProfileTitle = () => null
+export default ProfileTitle
